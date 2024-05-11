ab0aca3ea08901636a5238cd928af67b1a71cf1d
MarcosBizzarri <marcos-bizzarri@hotmail.com>
2024-05-11 13:23:52 -0300

    criando navbar

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarChuvaComponent } from './template/sidebar-chuva/sidebar-chuva.component';
 import { SidebarItemChuvaComponent } from './template/sidebar-chuva/sidebar-item-chuva/sidebar-item-chuva.component';
+import { NavbarChuvaComponent } from './template/navbar-chuva/navbar-chuva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarChuvaComponent,
-    SidebarItemChuvaComponent
+    SidebarItemChuvaComponent,
+    NavbarChuvaComponent
     
   ],
   imports: [
---
+++ ts/src/app/template/navbar-chuva/navbar-chuva.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-chuva',
+  templateUrl: './navbar-chuva.component.html',
+  styleUrls: ['./navbar-chuva.component.scss']
+})
+export class NavbarChuvaComponent {
+  
+}
